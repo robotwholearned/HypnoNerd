ab547f62e40e0cebbee40996f11007a7e3c98f8d
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-05-26 23:29:25 -0600

    Initial Commit

---
+++ HypnoNerd/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  HypnoNerd
+//
+//  Created by Sandquist, Cassandra G on 5/26/14.
+//  Copyright (c) 2014 robotwholearned. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
