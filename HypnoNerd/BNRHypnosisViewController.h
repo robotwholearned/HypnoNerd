b750d6587c5c602dc5d181a5a8075bbc451aaef9
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-05-26 23:32:04 -0600

    Add BNRHypnosisViewContoller

---
+++ HypnoNerd/BNRHypnosisViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisViewController.h
+//  HypnoNerd
+//
+//  Created by Sandquist, Cassandra G on 5/26/14.
+//  Copyright (c) 2014 robotwholearned. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisViewController : UIViewController
+
+@end
