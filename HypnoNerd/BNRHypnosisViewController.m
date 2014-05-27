2302ab12b8da9bd4d3373456aef2526b33d3a162
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-05-26 23:36:57 -0600

    load hypnosis view

--- HypnoNerd/BNRHypnosisViewController.m
+++ HypnoNerd/BNRHypnosisViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BNRHypnosisViewController.h"
+#import "BNRHypnosisView.h"
 
 @interface BNRHypnosisViewController ()
 
@@ -14,36 +15,10 @@
 
 @implementation BNRHypnosisViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (void)loadView
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+    CGRect frame = [[UIScreen mainScreen] bounds];
+    BNRHypnosisView* backgroundView = [[BNRHypnosisView alloc] initWithFrame:frame];
+    self.view = backgroundView;
 }
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
