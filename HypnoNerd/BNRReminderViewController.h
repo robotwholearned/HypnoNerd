9188d5b34787e49dc8a6cd45457cef462d4723af
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-05-26 23:40:50 -0600

    Add reminder view controller

---
+++ HypnoNerd/BNRReminderViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRReminderViewController.h
+//  HypnoNerd
+//
+//  Created by Sandquist, Cassandra G on 5/26/14.
+//  Copyright (c) 2014 robotwholearned. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRReminderViewController : NSObject
+
+@end
---
+++ HypnoNerd/BNRReminderViewController.m
@@ -0,0 +1,13 @@
+//
+//  BNRReminderViewController.m
+//  HypnoNerd
+//
+//  Created by Sandquist, Cassandra G on 5/26/14.
+//  Copyright (c) 2014 robotwholearned. All rights reserved.
+//
+
+#import "BNRReminderViewController.h"
+
+@implementation BNRReminderViewController
+
+@end
